d50f55c5804b7f4ea52e53fc434820361a07a58f
Kristina Sheremet <k.khasnutdinova@yandex.ru>
2021-05-21 13:15:36 +0500

    fix: fix error

--- src/scripts/components/user-info.js
+++ src/scripts/components/user-info.js
@@ -2,14 +2,12 @@ export default class UserInfo {
     constructor(name, job) {
     this._profileTitle = document.querySelector(name);
     this._profileSubtitle = document.querySelector(job);
-    this._profileTitle = document.querySelector('.profile__title')
-    this._profileSubtitle = document.querySelector('.profile__subtitle') 
     }
 
     getUserInfo() {
         const values = {
-            name: this._name = document.querySelector('.profile__title').textContent,
-            job: this._job = document.querySelector('.profile__subtitle').textContent
+            name: this._name = this._profileTitle.textContent,
+            job: this._job = this._profileSubtitle.textContent
         }
         
         return values
